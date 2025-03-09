e7c2caed7c27733319371591b2e1074e5292f405
Kojok120 <kojokamo120@gmail.com>
2025-03-10 03:29:43 +0900

    コントローラのテスト記載

--- test/controllers/tweets_controller_test.rb
+++ test/controllers/tweets_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class TweetsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get tweets_url
+    assert_response :success
+  end
+  
+  test "should get new" do
+    get new_tweet_url
+    assert_response :success
+  end
+  
+  test "should get show" do
+    # You'll need a tweet to show
+    tweet = tweets(:one) # Assuming you have fixtures
+    get tweet_url(tweet)
+    assert_response :success
+  end
+  
+  test "should get edit" do
+    tweet = tweets(:one) # Assuming you have fixtures
+    get edit_tweet_url(tweet)
+    assert_response :success
+  end
 end
