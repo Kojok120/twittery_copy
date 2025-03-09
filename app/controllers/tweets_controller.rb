4ae68e80619c37d6b334c17842dc30116ad62574
Kojok120 <kojokamo120@gmail.com>
2025-03-10 03:29:35 +0900

    コントローラ記載

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,47 @@
 class TweetsController < ApplicationController
+    def index
+        @tweets = Tweet.all.order(created_at: :desc)
+    end
+    
+    def new
+        @tweet = Tweet.new
+    end
+
+    def show
+        @tweet = Tweet.find(params[:id])
+    end
+    
+    def create
+        @tweet = Tweet.new(tweet_params)
+        if @tweet.save
+            redirect_to root_path, notice: "投稿しました"
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @tweet = Tweet.find(params[:id])
+    end
+
+    def update
+        @tweet = Tweet.find(params[:id])
+        if @tweet.update(tweet_params)
+            redirect_to @tweet, notice: "更新しました"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @tweet = Tweet.find(params[:id])
+        @tweet.destroy
+        redirect_to root_path, notice: "削除しました"
+    end
+    
+    private
+    
+    def tweet_params
+        params.require(:tweet).permit(:content)
+    end
 end
